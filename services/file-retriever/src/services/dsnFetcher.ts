b4caae1b497d79152d165da946f8b6083f12b2f1
Carlos Lostao <hastro1990@gmail.com>
2024-12-08 22:44:49 +0100

    update: file composer method

--- services/file-retriever/src/services/dsnFetcher.ts
+++ services/file-retriever/src/services/dsnFetcher.ts
@@ -5,6 +5,10 @@ import {
   decodeNode,
   MetadataType,
   cidToString,
+  CompressionAlgorithm,
+  COMPRESSION_CHUNK_SIZE,
+  cidOfNode,
+  decompressFile,
 } from '@autonomys/auto-dag-data'
 import { PBNode } from '@ipld/dag-pb'
 import { env } from '../utils/env.js'
@@ -13,28 +17,47 @@ import { safeIPLDDecode } from '../utils/dagData.js'
 import mime from 'mime-types'
 
 const SUBSPACE_GATEWAY_URL = env('SUBSPACE_GATEWAY_URL')
-const MAX_SIMULTANEOUS_FETCHES = 10
+const MAX_SIMULTANEOUS_FETCHES = Number(
+  env('MAX_SIMULTANEOUS_FETCHES', {
+    defaultValue: 10,
+  }),
+)
 
 const fetchNode = async (cid: string): Promise<PBNode> => {
-  const objectMappingHash = blake3HashFromCid(stringToCid(cid))
+  const objectMappingHash = Buffer.from(
+    blake3HashFromCid(stringToCid(cid)),
+  ).toString('hex')
   const response = await fetch(
-    `${SUBSPACE_GATEWAY_URL}/objects/${objectMappingHash}`,
+    `${SUBSPACE_GATEWAY_URL}/data/${objectMappingHash}`,
   )
   if (!response.ok) {
+    console.error('Failed to fetch node', response)
     throw new HttpError(500, 'Internal server error: Failed to fetch node')
   }
 
   const blob = await response.arrayBuffer()
 
-  return decodeNode(blob)
+  const node = decodeNode(blob)
+
+  if (cidToString(cidOfNode(node)) !== cid) {
+    throw new Error(`Cid mismatch: ${cid} !== ${cidToString(cidOfNode(node))}`)
+  }
+
+  return node
 }
 
 const createReadStream = (node: PBNode): ReadableStream => {
   const metadata = safeIPLDDecode(node)
+
   if (metadata?.data) {
     return new ReadableStream({
       start: async (controller) => {
-        controller.enqueue(metadata.data)
+        const asyncIterator = [Buffer.from(metadata.data!)]
+
+        for await (const chunk of asyncIterator) {
+          controller.enqueue(chunk)
+        }
+
         controller.close()
       },
     })
@@ -43,6 +66,7 @@ const createReadStream = (node: PBNode): ReadableStream => {
   return new ReadableStream({
     start: async (controller) => {
       let requestsPending = node.Links.map(({ Hash }) => cidToString(Hash))
+      console.log(`Initial links: ${requestsPending}`)
       while (requestsPending.length > 0) {
         const requestingNodes = requestsPending.slice(
           0,
@@ -50,27 +74,34 @@ const createReadStream = (node: PBNode): ReadableStream => {
         )
 
         const nodes = await Promise.all(
-          requestingNodes.map((e) => fetchNode(e)),
+          requestingNodes.map(async (e, sortIndex) => ({
+            sortIndex,
+            node: await fetchNode(e),
+          })),
         )
 
-        const links = nodes
-          .map((e) => {
-            const ipldMetadata = safeIPLDDecode(e)
+        const sortedNodes = nodes
+          .sort((a, b) => a.sortIndex - b.sortIndex)
+          .map((e) => e.node)
 
-            if (ipldMetadata?.type === MetadataType.FileChunk) {
-              controller.enqueue(ipldMetadata.data)
-              return []
-            } else {
-              return e.Links.map((e) => cidToString(e.Hash))
-            }
-          })
-          .flat()
+        let newLinks: string[] = []
+        for (const node of sortedNodes) {
+          const ipldMetadata = safeIPLDDecode(node)
+          if (ipldMetadata?.data) {
+            controller.enqueue(ipldMetadata.data)
+          } else {
+            newLinks = newLinks.concat(
+              node.Links.map((e) => cidToString(e.Hash)),
+            )
+          }
+        }
 
         requestsPending = [
-          ...links,
+          ...newLinks,
           ...requestsPending.slice(MAX_SIMULTANEOUS_FETCHES),
         ]
       }
+      controller.close()
     },
   })
 }
@@ -86,8 +117,21 @@ const fetchFile = async (cid: string): Promise<FileResponse> => {
     throw new HttpError(400, 'Bad request: Not a file')
   }
 
+  const isCompressedAndNotEncrypted =
+    nodeMetadata.uploadOptions?.encryption === undefined &&
+    nodeMetadata.uploadOptions?.compression?.algorithm ===
+      CompressionAlgorithm.ZLIB
+
+  const data = isCompressedAndNotEncrypted
+    ? decompressFile(createReadStream(head), {
+        algorithm: CompressionAlgorithm.ZLIB,
+        chunkSize: COMPRESSION_CHUNK_SIZE,
+        level: 9,
+      })
+    : createReadStream(head)
+
   return {
-    data: createReadStream(head),
+    data,
     size: nodeMetadata.size,
     mimeType: mime.lookup(nodeMetadata.name ?? '') || undefined,
     filename: nodeMetadata.name,
