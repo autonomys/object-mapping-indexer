cd36f03cfa8649cb2262cf2b7c43869198c6c75c
Carlos Lostao <hastro1990@gmail.com>
2024-12-12 14:48:34 +0100

    update: extract envvars to a config file

---
+++ services/file-retriever/src/config.ts
@@ -0,0 +1,25 @@
+import { env } from './utils/env'
+
+const TEN_GB = 10 * 1024 ** 3
+const ONE_DAY = 24 * 60 * 60 * 1000
+
+export const config = {
+  // Required
+  apiSecret: env('API_SECRET'),
+  subspaceGatewayUrl: env('SUBSPACE_GATEWAY_URL'),
+  // Optional
+  port: Number(env('FILE_RETRIEVER_PORT', { defaultValue: 8090 })),
+  corsOrigin: env('CORS_ORIGIN', { defaultValue: '*' }),
+  maxSimultaneousFetches: Number(
+    env('MAX_SIMULTANEOUS_FETCHES', {
+      defaultValue: 10,
+    }),
+  ),
+  cacheDir: env('CACHE_DIR', { defaultValue: './.cache' }),
+  cacheMaxSize: Number(
+    env('CACHE_MAX_SIZE', {
+      defaultValue: TEN_GB,
+    }),
+  ),
+  cacheTtl: Number(env('CACHE_TTL', { defaultValue: ONE_DAY })),
+}
